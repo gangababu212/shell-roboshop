2e80081496c7e42595ef42f40bf3c86d23089049
gangababu M <gangababumedisetty.com>
2026-02-07 20:41:48 +0530

    actions

---
+++ roboshop.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+SG-ID="sg-06264c970c7d1ae80"
+AMI-IS="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+  INSTANCE_ID=$( aws ec2 run-instance \
+  --image-id $AMI_ID\
+  --instnace-type"t3.micro" \
+  --security-group-ids $SG_ID \
+  --tag-sepecification "ResourceType=instance,Tags=[{key=Name,Value=$instnace}]" \
+  --query 'Instnace[0].InstnaceId' \
+  --output text )
+
+
+if [$instance == "frontend"]; then
+IP=$S
+   (aws ec2 describe-instance \
+   --instnace-ids $INSTANCE_ID \
+   --query 'Reservation[].Instance[].PublicIpAddress' \
+   --output text 
+   )
+else
+   IP=$S(aws ec2 describe-instance \
+   --instnace-ids $INSTANCE_ID \
+   --query 'Reservation[].Instance[].PrivateIpAddress' \
+   --output text 
+   )
+fi
+
+echo "IP Adress: $IP"
+
+aws route53 change-resource-recod-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch'
+    {
+        "Comment": "Updating record",
+        "Changes":[
+        {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+             "Name": "'$RECORD_NAME'",
+             "Type": "A",
+             "TTL": 1,
+             "ResourceRecords":[
+             {
+                "value": "'$IP'"
+             }
+             ]
+         }
+         }
+     ]
+}
+,
+
+echo "record update for $instance"
+
+done
